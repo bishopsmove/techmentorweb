558b43bf61ad7ccd58b50ead8c1b6a69aa418621
Rory Primrose <rprimrose@sigmabravo.com>
2017-09-29 23:28:01 +1000

    Updated client hosted configuration.

--- config/hosted.js
+++ config/hosted.js
@@ -8,6 +8,9 @@ module.exports = {
     environment: "#{Environment.Name}",
     version: "#{Release.Number}",
     apiUri: "#{Api.Uri}",
+
+    // Client configuration
+    authAudience: "#{Api.Uri}",
     
     // Server configuration
     port: 443,
